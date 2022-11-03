11727c02cf71f537629e97298fcf4eed6d66d368
miclegoden <darklerbrian1025@gmail.com>
2022-11-03 08:50:16 -0700

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,213 @@
+import portfolio_1 from '../src/assets/images/portfolio1.jpg'
+import portfolio_2 from '../src/assets/images/portfolio2.jpg'
+import portfolio_3 from '../src/assets/images/portfolio3.jpg'
+
+
+
+const logotext = 'Micle'
+const meta = {
+  title: 'Michael Gordon',
+  description:
+    'I’m Michael Gordon  _ Full stack devloper,currently working in Montreal',
+}
+
+const introdata = {
+  title: 'I’m Michael Gordon',
+  animated: {
+    first: 'I love coding',
+    second: 'I code cool websites',
+    third: 'I develop software application',
+  },
+  description:
+    'As a perfect web developer, have 8years experience of development, has fully demonstrated his skillful technology and development ability Credibility and completeness of development are important parts of me.',
+  your_img_url: 'https://images.unsplash.com/photo-1514790193030-c89d266d5a9d',
+}
+
+const dataabout = {
+  title: 'Education',
+  university: 'Keio University',
+  detail: 'From July 2013 to August 2015, Bachelor degree',
+}
+const worktimeline = [
+  {
+    jobtitle: 'Fullstack Developer',
+    where: 'Voiceflip, Canada',
+    date: '2022',
+  },
+  {
+    jobtitle: 'Python Software Engineer',
+    where: 'iTechArt, Belarus',
+    date: '2020 ~ 2021',
+  },
+  {
+    jobtitle: 'Senior Front-End Developer',
+    where: 'Apple Solve Company, Toronto, Canada',
+    date: '2017 ~ 2020',
+  },
+  {
+    jobtitle: 'Front-End Developer',
+    where: 'Pixelpoint Hive, Newport Beach, CA, United States',
+    date: '2015 ~ 2017',
+  },
+]
+
+const skills = [
+  {
+    name: 'Html | CSS | Javascript',
+    value: 100,
+  },
+
+  {
+    name: 'React | Jquery',
+    value: 95,
+  },
+  {
+    name: 'Typescript | Next.js | Redux',
+    value: 90,
+  },
+  {
+    name: 'Angular.js | Vue.js',
+    value: 90,
+  },
+  {
+    name: 'Bootstrap, Tailwind CSS, SASS',
+    value: 93,
+  },
+  {
+    name: 'Node | Express | Python',
+    value: 90,
+  },
+  {
+    name: 'PHP | Laravel',
+    value: 92,
+  },
+  {
+    name: 'MySql | MongoDB | Postql | Graphql',
+    value: 85,
+  },
+]
+
+const services = [
+  // {
+  //   title: 'UI & UX Design',
+  //   description:
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  // },
+  {
+    title: 'Frontend development',
+    description:
+      'Develop a beautiful and responsive frontend. Prioritize user needs and design creative and innovative',
+  },
+  {
+    title: 'Backend development',
+    description:
+      'Build a strong backend. Backend by flexible and quick response is favored by users',
+  },
+]
+
+const dataportfolio = [
+  {
+    img: portfolio_1,
+    desctiption:
+      'Frontend connect 20 more chains with web3, Post blog and store IPFS',
+    link: '#',
+  },
+  {
+    img:  portfolio_2,
+    desctiption:
+      'Responsible and web scrapping',
+    link: 'https://kaleidozdesign.com/',
+  },
+  {
+    img:  portfolio_3,
+    desctiption:
+      'Frontend about NFT with Material UI , React, Typescript, Sass',
+    link: 'https://doodcats.net/',
+  },
+  // {
+  //   img: 'https://picsum.photos/400/600/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+  // {
+  //   img: 'https://picsum.photos/400/300/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+  // {
+  //   img: 'https://picsum.photos/400/700/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+
+  // {
+  //   img: 'https://picsum.photos/400/600/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+  // {
+  //   img: 'https://picsum.photos/400/300/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+  // {
+  //   img: 'https://picsum.photos/400/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+  // {
+  //   img: 'https://picsum.photos/400/550/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+  // {
+  //   img: 'https://picsum.photos/400/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+  // {
+  //   img: 'https://picsum.photos/400/700/?grayscale',
+  //   desctiption:
+  //     'The wisdom of life consists in the elimination of non-essentials.',
+  //   link: '#',
+  // },
+]
+
+const contactConfig = {
+  YOUR_EMAIL: 'micgodtopdev@gmail.com',
+  YOUR_FONE: '+1 (724)740-9208',
+  description:
+    'If hire me, You can see satisfactory results ',
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: 'service_id',
+  YOUR_TEMPLATE_ID: 'template_id',
+  YOUR_USER_ID: 'user_id',
+}
+
+const socialprofils = {
+  github: 'https://github.com/miclegoden',
+  telegram: 'https://t.me/miclegoden',
+  discord: 'Justice#3389',
+  skype: 'live:.cid.9d08aec71d6488a4',
+}
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+}
